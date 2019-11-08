5b3dc8765cab7eb38cf1763c6850e6c5ced025c1
Matthew Schultz <mschult7@bingha>
2019-11-08 10:29:57 -0500

    additional navigation stuff

--- LeadScreenApp/LeadScreenApp/LeadScreenView.swift
+++ LeadScreenApp/LeadScreenApp/LeadScreenView.swift
@@ -8,33 +8,34 @@
 
 import SwiftUI
 
+
 struct LeadScreenView: View {
     @State private var selection = 0
- 
-        var body: some View {
-            VStack(spacing: 20) {
-            Text("Lead Screen")
-                .font(.title)
-//                Spacer()
-            Text("Choose a Lead Screening Resource")
-                .font(.subheadline)
-                
-                NavigationView{
-                    List{
-                        NavigationLink(destination: WHW()){
-                            Text("Who, How, When to Screen")
-                                .font(.body)
-                        }
+    
+    init(){
+            UITableView.appearance().tableFooterView = UIView()
+    }
+    var body: some View {
+        NavigationView{
+            VStack(spacing: 10){
+                //                Spacer()
+                Text("Choose a Lead Screening Resource")
+                    .font(.subheadline)
+                List{
+                    NavigationLink(destination: WHW()){
+                        Text("Who, How, When to Screen")
+                            .font(.body)
+                    }
                     NavigationLink(destination: WHW()){
                         Text("How to Manage Lead Levels")
                             .font(.body)
                     }
-
+                    
                     NavigationLink(destination: WHW()){
                         Text("Signs/Symptoms Lead Poisoning")
                             .font(.body)
                     }
-                    NavigationLink(destination: WHW()){
+                    NavigationLink(destination: ExposureSurvey()){
                         Text("Lead Exposure Risk Survey")
                             .font(.body)
                     }
@@ -42,12 +43,12 @@ struct LeadScreenView: View {
                         Text("High Lead Level Evaluation")
                             .font(.body)
                     }
-
-                    }
-                }
+                    
+                } .navigationBarTitle(Text("Lead Screen"), displayMode: .inline)
+            }
             
         }
-
+        
     }
 }
 
--- LeadScreenApp/LeadScreenApp/WHW.swift
+++ LeadScreenApp/LeadScreenApp/WHW.swift
@@ -8,18 +8,31 @@
 
 import SwiftUI
 
+
 struct WHW: View {
     @State private var selection = 0
- 
-        var body: some View {
-        VStack(alignment: .leading) {
-            Text("Who How When")
-                .font(.title)
-//                Spacer()
-
+    
+    init(){
+            UITableView.appearance().tableFooterView = UIView()
+    }
+    var body: some View {
+        NavigationView{
+            VStack(spacing: 20){
+                Text("\n")
+                Text("NYS Dept of Health requires:\n- Test all children at ages 12 and 24 months with a blood test.\n- Assess all children ages 6 months to 6 years at every well child visit for risk of lead exposure with Lead Exposure Risk Survey below.\n- Test all foreign-born children up to age 16 years, particularly refugee and internationally adopted children, upon arrival in the US and again 3-6 months after they obtain permanent residences.\n- Test children of any age if lead exposure suspected.")
+                    .font(.subheadline)
+                List{
+                    NavigationLink(destination: ExposureSurvey()){
+                        Text("Lead Exposure Risk Survey")
+                            .font(.body)
+                    }
+                   
+                    
+                } .navigationBarTitle(Text("Who How When"), displayMode: .inline)
+            }
             
         }
-        .padding()
+        
     }
 }
 
