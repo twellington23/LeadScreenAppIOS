e0d996d65f831efe13ea67f9285bc95bf201199c
meghamkpatel <32498703+meghamkpatel@users.noreply.github.com>
2019-11-13 14:25:06 -0500

    Update Ven5.swift

--- LeadScreenApp/LeadScreenApp/Ven5.swift
+++ LeadScreenApp/LeadScreenApp/Ven5.swift
@@ -21,12 +21,12 @@ struct Ven5: View {
                 Text("").font(.headline)
                 Text("-Test all children at age 1 year and again at age 2 years, regardless of initial result.\n-If child <6 years, perform a Lead Exposure Risk Assessment (see p. 1) at every well child visit, and test again if lead risk is found.\n- Provide anticipatory guidance4 to parent or guardian regarding major sources of lead exposure and ways to prevent exposure.")
                     .font(.subheadline)
-                Button(action: {
+//                 Button(action: {
 
-                    UIApplication.shared.openURL(URL(string: "http://health.ny.gov/publications/2526.pdf")!)
-                }) {
-                Text("health.ny.gov/publications/2526.pdf").font(.subheadline)
-                }
+//                     UIApplication.shared.openURL(URL(string: "http://health.ny.gov/publications/2526.pdf")!)
+//                 }) {
+//                 Text("health.ny.gov/publications/2526.pdf").font(.subheadline)
+//                 }
                    
                     
                 } .navigationBarTitle(Text("Venous: <5 ug/dL"), displayMode: .inline).padding()
