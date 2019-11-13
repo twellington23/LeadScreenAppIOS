664ff118ae9a9f0403407b262923f9edd300ddf3
meghamkpatel <32498703+meghamkpatel@users.noreply.github.com>
2019-11-13 15:17:33 -0500

    Update Ven44.swift

--- LeadScreenApp/LeadScreenApp/Ven44.swift
+++ LeadScreenApp/LeadScreenApp/Ven44.swift
@@ -19,13 +19,33 @@ struct Ven44: View {
         ScrollView{
             VStack(spacing: 10){
                 Text("").font(.headline)
-                Text("How soon confirm with venous testing?\n- Venous test within 1 week.\n\nManagement\n- Urge prompt venous confirmation testing as noted above.\n- Further management driven by result of confirmatory venous blood lead level.\n- Provide education such as 'What Your Child's Blood Lead Test Means' at")
+                Text("AFTER CONFIRMED VENOUS TEST, all activities above AND:\n- Perform a Clinical Lead Exposure Assessment\n- Provide lead exposure risk reduction education\n- Consider the child at risk for developmental delays and behavior concerns and provide ongoing developmental surveillance with prompt referrals for developmental services if needed\n- Test all children who spend time in the home and refer pregnant women in the home for testing\n -Coordinate care with local or state health department including environmental education and management\n- Notify family of the need for follow-up venous testing on a periodic basis\n- Frequency of follow-up testing for children with previous blood lead level elevations are best guided through consultation with a Regional Lead Resource Center\n- Consider consulting with a Regional Lead Resource Center")
                     .font(.subheadline)
-                Button(action: {
+                List{
+                     Button(action: {
 
-                    UIApplication.shared.openURL(URL(string: "http://health.ny.gov/publications/2526.pdf")!)
-                }) {
-                Text("health.ny.gov/publications/2526.pdf").font(.subheadline)
+                    UIApplication.shared.openURL(URL(string: "www.health.ny.gov/publications/2526.pdf")!)
+                    }) {
+                    Text("What Your Child’s Blood Lead Test Means").font(.subheadline)
+                    }
+                    Button(action: {
+
+                        UIApplication.shared.openURL(URL(string: "www.health.ny.gov/publications/2594.pdf")!)
+                    }) {
+                    Text("Lead Poisoning is a Danger for Every Baby and Child").font(.subheadline)
+                    }
+                    Button(action: {
+
+                        UIApplication.shared.openURL(URL(string: "www.health.ny.gov/environmental/lead")!)
+                    }) {
+                    Text("NYSDOH Website").font(.subheadline)
+                    }
+                    Button(action: {
+
+                        UIApplication.shared.openURL(URL(string: "www.health.ny.gov/environmental/lead/resource_centers.htm.")!)
+                    }) {
+                    Text("Regional Lead Resource Center").font(.subheadline)
+                    }
                 }
                     
                 } .navigationBarTitle(Text("Venous: 25-44 ug/dL"), displayMode: .inline).padding()
