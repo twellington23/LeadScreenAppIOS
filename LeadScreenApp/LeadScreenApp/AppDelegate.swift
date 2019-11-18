424a1b3d524f988d96530678c4da3deab1bd94af
Matthew Schultz <mschult7@bingha>
2019-11-18 09:59:59 -0500

    removed old alerts code

--- LeadScreenApp/LeadScreenApp/AppDelegate.swift
+++ LeadScreenApp/LeadScreenApp/AppDelegate.swift
@@ -16,25 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        let alert = AlertController()
-        alert.showAlert()
-        
-//        let alertController = UIAlertController(title: "Alert", message: "This is an alert.", preferredStyle: UIAlertController.Style.alert)
-//        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: nil))
-//        UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: true, completion: nil)
-//
-//        alertController.present(alertController, animated: true, completion: nil)
-//
-//        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
-//        alertWindow.rootViewController = UIViewController()
-//        alertWindow.windowLevel = UIWindow.Level.alert + 1;
-//        alertWindow.makeKeyAndVisible()
-//        alertWindow.rootViewController?.present(alertController, animated: true, completion: nil)
-//
-//        let alertController = UIAlertController(title: "Alert", message: "This is an alert.", preferredStyle: .alert)
-//        alertController.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
-//        alertController.present(alertController, animated: true, completion: nil)
-
+       
         return true
     }
 
